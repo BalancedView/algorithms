f0ca63f3420b3440455630cc374810c4e82295d5
Stefano Schiavi <stefano@bvprojects.org>
2015-03-30 00:20:20 +0200

    update scheduling_algo to be more functional

--- greedy/scheduling_algo.py
+++ greedy/scheduling_algo.py
@@ -1,32 +1,35 @@
 """This is a simple and fast implementation of a scheduling algorithm
-   The algorithm reads one line at a time and applies a line_fn to each
-   line to be processed (in this case we convert to int).
-   The file_gen is used as an iterable and passed to the built in sorted()
-   along with lambda as key to be used for sorting
+   The algorithm uses 2 generators to read lines from a file
+   the file_gen simply opens a file and reads one line at a time
+   the line_to_int_list as it says converts a line to a list of ints.
+   The lines_from_file is a generator used as an iterable passed to the
+   built in sorted() along with a lambda as key to be used for sorting
    In this case we sort using the difference 'job_weight - job_time' and
    in case of a tie we sort by largest weight
    Note this is not an optimal algorithm, it only serves the purpose of
    an exercise!
    once we have a job_list we can use the built in reduce to calculate
-   total_time and total_weight
+   total_time and total_weight using the calc_weight callback function
 """
 
-def line_to_int_tuple(line):
-    return tuple(int(i) for i in line.split())
+def line_to_int_list(line_seq):
+    for line in line_seq:
+        yield [int(i) for i in line.split()]
 
-def file_gen(file_name, line_fn):
+def file_gen(file_name):
     with open(file_name) as myfile:
         for line in myfile:
-            int_tuple = line_fn(line)
-            yield int_tuple
+            yield line
 
 def calc_weight(time_weight_dict, job):
     time_weight_dict['total_time'] += job[1]
     time_weight_dict['total_weight'] += time_weight_dict['total_time'] * job[0]
     return time_weight_dict
 
-job_list = sorted(file_gen('../test_data/greedy/jobs.txt', line_to_int_tuple),
-                  key=lambda job_list: (-(job_list[0] - job_list[1]),
-                                         -job_list[0]))
+int_lists_from_file = line_to_int_list(file_gen('../test_data/greedy/jobs.txt'))
+
+job_list = sorted(int_lists_from_file,
+                  key=lambda job: (-(job[0] - job[1]),
+                                   -job[0]))
 
 print(reduce(calc_weight, job_list, {'total_time': 0, 'total_weight': 0}))
